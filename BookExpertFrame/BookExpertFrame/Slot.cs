115116ef6742335413e31f7f57407fbbacf6412a
DashaYulbarisova <dyulbarisova@gmail.com>
2016-12-21 22:41:13 +0500

    description classes Frame, Slot

--- BookExpertFrame/BookExpertFrame/Slot.cs
+++ BookExpertFrame/BookExpertFrame/Slot.cs
@@ -9,6 +9,18 @@ namespace BookExpertFrame
     public class Slot
     {
         public string nameSlot;
-        public string defaultValue;                           
+        public string typeInheritance;
+        public string typeData;
+        public string valueSlot;
+        public string AttachedProcedure;
+
+        public Slot(string name, string inheritance, string data, string value, string procedure)
+        {
+            nameSlot = name;
+            typeInheritance = inheritance;
+            typeData = data;
+            valueSlot = value;
+            AttachedProcedure = procedure;
+    }
     }
 }
