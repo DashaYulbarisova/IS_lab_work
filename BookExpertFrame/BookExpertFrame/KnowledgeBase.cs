05055c4dc2bbb7b0368bd914e678ac0bc2e24732
DashaYulbarisova <dyulbarisova@gmail.com>
2016-12-29 01:50:17 +0500

    28.12

--- BookExpertFrame/BookExpertFrame/KnowledgeBase.cs
+++ BookExpertFrame/BookExpertFrame/KnowledgeBase.cs
@@ -6,18 +6,57 @@ using System.Threading.Tasks;
 
 namespace BookExpertFrame
 {
+
+
     public class KnowledgeBase : IKnowledgeBase
     {
         public List<Frame> listFrame;
         public List<Slot> listSlot;
 
+        private void CreateNewSlot(int type)
+        {
+            switch (type)
+            {
+                case 1:
+                    listSlot.Add(new Slot("назначение", "U", "", "", ""));
+                    listSlot.Add(new Slot("название", "U", "", "", ""));
+                    listSlot.Add(new Slot("автор", "U", "", "", ""));
+                    listSlot.Add(new Slot("объём", "U", "", "", ""));
+                    break;
+                case 2:
+                    listSlot.Add(new Slot("назначение", "U", "string", "в дорогу", ""));
+                    listSlot.Add(new Slot("название", "U", "string", "", ""));
+                    listSlot.Add(new Slot("автор", "U", "frame", "", "")); //должна быть процедура
+                    listSlot.Add(new Slot("объём", "unique", "string", "до 200 страниц", ""));
+                    break;
+                case 3:
+                    listSlot.Add(new Slot("назначение", "U", "string", "дома", ""));
+                    listSlot.Add(new Slot("название", "U", "", "", ""));
+                    listSlot.Add(new Slot("автор", "U", "frame", "", ""));
+                    listSlot.Add(new Slot("объём", "unique", "string", "более 200 страниц", ""));
+                    break;
+                case 4:
+                    listSlot.Add(new Slot("ФИО", "U", "string", "", ""));
+                    listSlot.Add(new Slot("жанр", "U", "", "", ""));
+                    break;
+            }
+        }
+
         public KnowledgeBase()
         {
-            listSlot.Add(new Slot("назначение", "unique", "", "", ""));
-            listSlot.Add(new Slot("название", "unique", "", "", ""));
-            listSlot.Add(new Slot("автор", "unique", "", "", ""));
-            listSlot.Add(new Slot("объём", "unique", "", "", ""));
+            CreateNewSlot(1);
             listFrame.Add(new Frame("книги", listSlot, null));
+            listSlot.Clear();
+            CreateNewSlot(2);
+            listFrame.Add(new Frame("книга в дорогу", listSlot, "IS_A"));
+            listSlot.Clear();
+            CreateNewSlot(3);
+            listFrame.Add(new Frame("книга дома", listSlot, "IS_A"));
+            listSlot.Clear();
+            CreateNewSlot(4);
+            listFrame.Add(new Frame("автор", listSlot, null));
+            listSlot.Clear();
+
         }
         public Frame getFrameByName(string nameFrame) // ДАША!!!!!!!!!!!
         {
---
+++ Choose Book Expert System/WinFormsBookExpertSystem/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFormsBookExpertSystem;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestisFactInWorkMemory()
+        {
+            Fact FakeFact = new Fact("Fake", "");
+        }
+    }
+}
